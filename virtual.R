e5c1a28bfd0901b4765c77f7e1782d2566364ee5
KLKua <khelokkua@gmail.com>
2023-11-10 01:03:10 +0800

    Generate virtual species month by month

--- virtual.R
+++ virtual.R
@@ -7,30 +7,28 @@ library(yaml)
 
 set.seed(42)
 # functions
-generate_envall <- function(env_list, extent_binary, env_inf){
+generate_envall <- function(env_list, extent_binary, medium_inf){
   df <- data.frame('env' = character(0), 'mean' = numeric(0), 'sd' = numeric(0))
   for(env in env_list){
     result_env <- raster(extent_binary)
     values(result_env) <- 0
     value <- c()
     for(t in time){
-      # t : 'yyyy-mm-dd'
-      # t need to be changed to 'yyyy-mm'
-      t_ <- sprintf('%s-%s', strsplit(t, '-')[[1]][1], strsplit(t, '-')[[1]][2])
-      rst <- raster(env_inf$info[[env]][[t_]]$tif_span_avg)
+      rst <- raster(medium_inf[[env]][[t]])
       value <- c(value, values(rst)[values(extent_binary) == 1])
       result_env <- result_env + rst
     }
-    df[nrow(df)+1, ] <- c(env, mean(value), sd(value))
+    df[nrow(df)+1, ] <- c(env, mean(value, na.rm = TRUE), sd(value, na.rm = TRUE))
     result_env <- result_env / length(time)
-    writeRaster(result_env, file.path('virtual', version, sprintf('%s_1yr_avg.tif', env)), overwrite = T)
+    writeRaster(result_env, file.path('virtual', version, sprintf('%s_avg.tif', env)), overwrite = T)
   }
   write.csv(df, file.path('virtual', version, 'env_inf.csv'), row.names = FALSE)
   
   # load last 1 yr average data
   df <- read.csv(file.path('virtual', version, 'env_inf.csv'), row.names = 1)
   for(env in env_list){
-    env_rst <- (raster(file.path('virtual', version, sprintf('%s_1yr_avg.tif', env))) - df[env, 'mean']) / df[env, 'sd']
+    env_rst <- (raster(file.path('virtual', version, sprintf('%s_avg.tif', env))) - df[env, 'mean']) / df[env, 'sd']
+    values(env_rst)[values(extent_binary) == 0] <- NA
     assign(paste0(env, '_all'), env_rst)
   }
   rsts <- lapply(env_list, function(env_list) get(paste0(env_list, "_all")))
@@ -38,14 +36,18 @@ generate_envall <- function(env_list, extent_binary, env_inf){
   names(env_all) <- env_list
   return(list(df = df, env_all = env_all))
 }
-generate_env_pca <- function(virtual_conf, env_all, time, env_list, env_inf){
+generate_env_pca <- function(virtual_conf, env_all, time, env_list, df, medium_inf){
   if(virtual_conf$env_pca == 'all'){
     env_pca <- list(env_pca = env_all)
   }
   if(virtual_conf$env_pca == 'random'){
     time_random <- sample(time, 1)
-    time_random_ <- sprintf('%s-%s', strsplit(time_random, '-')[[1]][1], strsplit(time_random, '-')[[1]][2])
-    rsts_random <- lapply(env_list, function(env_list) (raster(env_inf$info[[env_list]][[time_random_]]$tif_span_avg) - df[env_list, 'mean']) / df[env_list, 'sd'])
+    rsts_random <- lapply(env_list, function(env_list) {
+      rst_normalize <- (raster(medium_inf[[env_list]][[time_random]]) - df[env_list, 'mean']) / df[env_list, 'sd']
+      values(rst_normalize)[is.na(values(rst_normalize))] <- 0
+      values(rst_normalize)[values(extent_binary) == 0] <- NA
+      return(rst_normalize)
+      })
     env_random <- stack(rsts_random)
     names(env_random) <- env_list
     env_pca <- list(env_pca = env_random, time_random = time_random)
@@ -80,6 +82,7 @@ write_yaml(virtual_conf, sprintf('virtual/%s/virtual_conf_%s.yaml', version, ver
 # load environmental information 
 env_inf <- fromJSON(file = virtual_conf$meta_json_files$env_inf)
 k_inf <- fromJSON(file = virtual_conf$meta_json_files$k_inf)
+medium_inf <- fromJSON(file = virtual_conf$meta_json_files$medium)
 
 # load DeepSDM model configurations
 env_list <- sort(virtual_conf$env_list)
@@ -89,7 +92,7 @@ time <- virtual_conf$time
 prevalence <- virtual_conf$prevalence
 
 # generate env_all
-output <- generate_envall(env_list, extent_binary, env_inf)
+output <- generate_envall(env_list, extent_binary, medium_inf)
 df <- output$df
 env_all <- output$env_all
 
@@ -103,7 +106,7 @@ for(i_sp in 1:virtual_conf$num_species){
   sp <- paste0('sp', sprintf('%02d', i_sp))
   
   # differenct virtual_conf$env_pca
-  env_pca <- generate_env_pca(virtual_conf, env_all, time, env_list, env_inf)
+  env_pca <- generate_env_pca(virtual_conf, env_all, time, env_list, df, medium_inf)
   
   # use 21 year average environment to conduct PCA
   random_sp <- generateRandomSp(env_pca$env_pca, 
@@ -137,10 +140,12 @@ for(i_sp in 1:virtual_conf$num_species){
   #specific time data
   for(t in time){
     # t <- time[1]
-    # t : 'yyyy-mm-dd'
-    # t need to be changed to 'yyyy-mm'
-    t_ <- sprintf('%s-%s', strsplit(t, '-')[[1]][1], strsplit(t, '-')[[1]][2])
-    rsts_time <- lapply(env_list, function(env_list) (raster(env_inf$info[[env_list]][[t_]]$tif_span_avg) - df[env_list, 'mean']) / df[env_list, 'sd'])
+    rsts_time <- lapply(env_list, function(env_list){
+      rst_normalize <- (raster(medium_inf[[env_list]][[t]]) - df[env_list, 'mean']) / df[env_list, 'sd']
+      values(rst_normalize)[is.na(values(rst_normalize))] <- 0
+      values(rst_normalize)[values(extent_binary) == 0] <- NA
+      return(rst_normalize)
+      })
     env_time <- stack(rsts_time)
     names(env_time) <- env_list
     
